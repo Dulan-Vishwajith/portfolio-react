9a6f2079c73c0c78f963ecd8f73af1010ab283c2
Dulan Vishwajith <dulanvishwajithgunarathna@gmail.com>
2026-04-21 22:33:51 +0530

    Add functionas for contacts section and improve it

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,38 +1,48 @@
 import './Contacts.css'
 import ContactForm from './ContactForm';
+
 function Contact() {
-  
   return (
-        <section className="footer" id="contact">
-            {/*Footer Container*/}
-            <div className="footer-container">
-                
-                {/*Contact Section*/}
-                <a href="#contact" className='title-button'><h3 id="contact-title">GET IN TOUCH</h3></a>
+    <section className="footer" id="contact">
+        {/* Footer Main */}
+      <div className="footer-main">
+        {/* Contact Information */}
+        <div className="footer-container">
+            <a href="#contact" className='title-button'>
+                <h3 id="contact-title">GET IN TOUCH</h3>
+            </a>
 
-                {/*Contact Details*/}
-                <div className="contact">
-                    {/*Contact Title*/}
-                    <h2>Let's Connect...</h2>
-
-                    {/*Email*/}
-                    <div className="email">
-                        <a href="mailto:dulanvishwajithgunarathna@gmail.com"><i className="fas fa-envelope"></i> Email Me</a>
-                    </div>
-                    {/*Social Links*/}
+            {/* Contact Links */}
+            <div className="contact">
+                <h2>Let's Connect...</h2>
+                    {/* Social Media and Contact Links */}
                     <div className="links">
-                        <a href="https://github.com/Dulan-Vishwajith" target="_blank"><i className="fab fa-github"></i>  GitHub</a>
-                        <a href="https://www.linkedin.com/in/dulan-vishwajith-a8a230216/"><i className="fab fa-linkedin"></i>  LinkedIn</a>
-                    </div>                    
-
-                </div>
-
+                    <a href="[github.com](https://github.com/Dulan-Vishwajith)" target="_blank" rel="noreferrer">
+                        <i className="fab fa-github"></i> GitHub
+                    </a>
+                    <a href="[linkedin.com](https://www.linkedin.com/in/dulan-vishwajith-a8a230216/)" target="_blank" rel="noreferrer">
+                        <i className="fab fa-linkedin"></i> LinkedIn
+                    </a>
+                    <a href="mailto:dulanvishwajithgunarathna@gmail.com">
+                        <i className="fas fa-envelope"></i> Email Me
+                    </a>
+                    </div>
             </div>
+        </div>
 
-            <ContactForm />
-            
-        </section>
-    );
+        {/* Contact Form */}
+        <ContactForm />
+
+      </div>
+
+        {/* Footer Bottom */}
+        <div className="footer-bottom">
+            {/* Copyright Notice */}
+            <p>© 2026 Dulan Vishwajith. All rights reserved.</p>
+        </div>
+
+    </section>
+  );
 }
 
 export default Contact;
